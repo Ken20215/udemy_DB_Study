74df27055ac2e7c68014b7b52453005fe130808d
Ken20215 <k.ishioka@renk.co.jp>
2025-09-08 15:30:12 +0900

    Section4_28 昇順・降順、表示制限

---
+++ Section4/Section4_28.sql
@@ -0,0 +1,49 @@
+SELECT DATABASE();
+
+use my_db;
+
+SHOW TABLES;
+
+DESCRIBE people;
+
+# カラム追加
+ALTER TABLE people ADD age INT AFTER name;
+
+# データ追加
+INSERT INTO people VALUES(1, "John",18, "2001-01-01");
+INSERT INTO people VALUES(2, "Alice",15, "2003-01-01");
+INSERT INTO people VALUES(3, "Paul",19, "2001-01-01");
+INSERT INTO people VALUES(4, "Chris",17, "2001-01-01");
+INSERT INTO people VALUES(5, "Vette",20, "2001-01-01");
+INSERT INTO people VALUES(6, "Tsuyoshi",21, "2001-01-01");
+
+# テーブル中身確認
+SELECT * FROM people;
+
+# ageで昇順
+SELECT * FROM people ORDER BY age;
+
+# ageで降順
+SELECT * FROM people ORDER BY age DESC;
+
+# nameで降順
+SELECT * FROM people ORDER BY name DESC;
+
+# 二つのカラムで昇順、降順
+SELECT * FROM people ORDER BY birth_day DESC, name DESC, age ASC;
+
+# ASC: 昇順
+# DESC: 降順
+
+# DISNTICT
+SELECT DISTINCT birth_day FROM people ORDER BY birth_day;
+SELECT DISTINCT name, birth_day FROM people;
+
+# LIMITは最初の行だけ表示
+SELECT id, name, age FROM people LIMIT 3;
+
+# 飛ばして表示(3飛ばし2表示)
+SELECT * FROM people LIMIT 3,2;
+
+SELECT * FROM people LIMIT 4 OFFSET 2;
+
