815068a19007f193ce1b9ed4dec71fa5151557fa
Ken20215 <k.ishioka@renk.co.jp>
2025-09-08 15:10:08 +0900

    カラムの削除、追加、名前変更、位置変更、主キー削除

--- Section3/Section3_24.sql
+++ Section3/Section3_24.sql
@@ -1,33 +1,37 @@
-USE my_db;
-
 SELECT DATABASE();
-
-# テーブル作成
-CREATE TABLE users(
-id INT, -- idカラムINT型
-name VARCHAR(10), -- 名前、可変長文字列
-age INT,
-phone_number CHAR(13), -- 固定長
-message TEXT
-)
-
-# テーブル一覧表示
-SHOW TABLES;
-
-#　テーブルの定義確認
-DESCRIBE users;
-
+SHOW DATABASES;
 USE my_db;
 
+# テーブル一覧
 SHOW TABLES;
+# テーブル名変更（RENAME TO）
+ALTER  TABLE users RENAME TO users_table;
+# カラムの削除（DROP COLUMN）
+DESCRIBE users_table;
 
-# テーブル作成
-CREATE TABLE users(
-id INT PRIMARY KEY, -- idカラムINT型
-name VARCHAR(10), -- 名前、可変長文字列
-age INT,
-phone_number CHAR(13), -- 固定長
-message TEXT
-)
+ALTER TABLE users_table DROP COLUMN message;
 
-DESCRIBE users;
+# カラムの追加（ADD）
+ALTER TABLE users_table ADD post_code CHAR(8);
+
+ALTER TABLE users_table ADD gender CHAR(1) AFTER age;
+
+# 一番最初のカラムに追加
+ALTER TABLE users_table ADD new_id INT FIRST;
+
+# カラム削除
+ALTER TABLE users_table DROP COLUMN new_id ;
+
+# カラムの定義変更
+ALTER TABLE users_table MODIFY name VARCHAR(50);
+
+#　カラム名変更
+ALTER TABLE users_table CHANGE COLUMN name 名前 VARCHAR(50);
+
+# カラム位置変更
+ALTER TABLE users_table CHANGE COLUMN gender gender CHAR(1) AFTER post_code;
+
+#主キーの削除
+ALTER TABLE users_table DROP PRIMARY KEY;
+
+DESCRIBE users_table;
