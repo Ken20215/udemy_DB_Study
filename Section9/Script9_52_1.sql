3fdcf6cbea8da53dbe6069906b09f615db3e81df
Ken20215 <k.ishioka@renk.co.jp>
2025-10-21 14:31:43 +0900

    Section9_52 トランザクションとロック

---
+++ Section9/Script9_52_1.sql
@@ -0,0 +1,63 @@
+SHOW DATABASES;
+USE day_4_9_db;
+
+
+SHOW TABLES;
+
+SELECT * FROM customers;
+
+-- 主キーでUPDATE(行ロック)
+UPDATE customers SET age=43 WHERE id=1;
+ROLLBACK;
+
+-- 主キーの指定なしでテーブル全体のロックがされます
+START TRANSACTION;
+UPDATE customers SET age=42 WHERE name="河野 文典";
+ROLLBACK;
+
+-- DELETE 
+START TRANSACTION;
+DELETE FROM customers WHERE id=1;
+COMMIT;
+
+-- INSERT
+ALTER TABLE customers DROP COLUMN name_length;
+START TRANSACTION;
+INSERT INTO customers VALUES(1,"田中 一郎", 21, "1991-01-10");
+SELECT * FROM customers;
+ROLLBACK;
+
+
+-- SELECTのロック
+-- FOR SHARE(共有ロック)
+-- FOR UPDATE(排他ロック)
+START TRANSACTION;
+SELECT * FROM customers WHERE id=1 FOR SHARE;
+ROLLBACK ;
+
+
+START TRANSACTION;
+SELECT * FROM customers WHERE id=1 FOR UPDATE;
+ROLLBACK ;
+
+-- LOCK TABLE READ
+LOCK TABLE customers READ;
+SELECT * FROM customers;
+UPDATE customers SET age=42 WHERE id=1;
+
+UNLOCK TABLES;
+
+-- LOCK TABLE WRITE
+ LOCK TABLE customers WRITE;
+SELECT * FROM customers;
+UPDATE customers SET age=42 WHERE id=1;
+
+UNLOCK TABLES;
+
+
+-- DEAD LOCK 
+START TRANSACTION;
+-- customers ⇨ users
+UPDATE customers SET age=42 WHERE id=1;
+UPDATE users SET age=12 WHERE id=1;
+
---
+++ Section9/Script9_52_2.sql
@@ -0,0 +1,17 @@
+USE day_4_9_db;
+UPDATE customers SET age=44 WHERE id=2;
+SELECT * FROM customers;
+
+UPDATE customers SET age=44 WHERE id=1;
+SELECT * FROM customers WHERE id=1 FOR SHARE;
+
+
+SELECT * FROM customers;
+UPDATE customers SET age=42 WHERE id=1;
+
+START TRANSACTION;
+-- users ⇨ customers
+UPDATE users SET age=12 WHERE id=1;
+UPDATE customers SET age=42 WHERE id=1;
+
+UNLOCK TABLES;
