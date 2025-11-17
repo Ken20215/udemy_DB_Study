f7004427592f77fef73797e6ec9ab41aaa582cc0
Ken20215 <k.ishioka@renk.co.jp>
2025-11-17 13:05:13 +0900

    Section10_55 集計関数

---
+++ Section10/Section10_55.sql
@@ -0,0 +1,38 @@
+USE day_4_9_db;
+SHOW TABLES;
+
+SELECT * FROM customers WHERE name IS NULL;
+
+-- COUNT
+SELECT COUNT(*) FROM customers; -- 何行データか
+SELECT COUNT(name) FROM customers; -- 列指定（この行に何行入っているか(NULLはカウントしない)）
+
+SELECT COUNT(name) FROM customers WHERE id>80;
+
+-- MAX: 最大、MIN: 最小
+SELECT MAX(age)c, MIN(age) FROM users WHERE birth_place="日本";
+SELECT MAX(birth_day), MIN(birth_day) FROM users;
+
+-- SUM: 合計値
+SELECT SUM(salary) FROM employees;
+SELECT AVG(salary) FROM employees;
+
+-- AVG:NULLの場合が面倒
+CREATE TABLE tmp_count(
+ num INT
+);
+
+SHOW TABLES;
+
+INSERT INTO tmp_count VALUES(1);
+INSERT INTO tmp_count VALUES(2);
+INSERT INTO tmp_count VALUES(3);
+INSERT INTO tmp_count VALUES(NULL);
+SELECT * FROM tmp_count;
+
+SELECT AVG(COALESCE (num, 0)) FROM tmp_count;
+
+
+
+
+
